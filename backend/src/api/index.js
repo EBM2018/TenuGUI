8b2a46e5c5ffe81753c4dc7ec2333aff10286429
Lenophie <quasiphies@gmail.com>
2019-02-11 10:55:32 +0100

    Lay out basic API

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,32 +3,16 @@ const { Router } = require('express');
 const router = new Router();
 
 /**
- * @api {get} / Hello World
- * @apiName GetHome
+ * @api {get} / Hello APIGUI
+ * @apiName GetApiHome
  * @apiGroup Static Pages
- * @apiDescription Cette URL affiche un simple message Hello World
- *
- * Il est possible d'écrire des messages sur plusieurs lignes dans la description.
+ * @apiDescription This URL displays a simple Hello
  * @apiSuccessExample {html} Success-Response:
  *     HTTP/1.1 200 OK
- *     Hello, World!
+ *     Hello, je suis l'APIGUI!
  */
-router.get('/', (req, res) => res.send('Hello, World!'));
+router.get('/', (req, res) => res.send('Hello, I am APIGUI!'));
 
-/**
- * @api {get} /:name Say hello to a specific name
- * @apiName GetName
- * @apiGroup Static Pages
- * @apiDescription Cette URL affiche un message Hello personnalisé
- *
- * @apiParam  {String} name Nom de la personne à saluer
- * @apiParamExample  {String} Request-Example:
-     name: Nymous
- *
- * @apiSuccessExample {html} Success-Response:
-     HTTP/1.1 200 OK
-     Hello, Nymous!
- */
-router.get('/:name', (req, res) => res.send(`Hello, ${req.params.name}!`));
+router.use('/fishtanks', require('./fishtanks'));
 
 module.exports = router;
