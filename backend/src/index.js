bc583a4c6c863173a73cd7a20922860c5cffd4a6
Lenophie <quasiphies@gmail.com>
2019-02-12 14:13:38 +0100

    Fix indent

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,10 @@ app.use('/api', require('./api'));
 app.use(serveStatic('./public'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 server.listen(config.app.port, (err) => {
-  if (err) console.error(err);
-  else console.log(`Listening on port ${config.app.port}`);
+    if (err) console.error(err);
+    else console.log(`Listening on port ${config.app.port}`);
 });
