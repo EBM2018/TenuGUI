caa8de20151dcca9fe09ce40d8c09a5d6b7cf611
Lenophie <quasiphies@gmail.com>
2019-02-13 10:30:40 +0100

    Enrich Fishtank model

--- backend/src/database/migrations/20190212091815-create-fishtank.js
+++ backend/src/database/migrations/20190212091815-create-fishtank.js
@@ -1,27 +1,37 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Fishtanks', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      ownerId: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Fishtanks');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Fishtanks', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            ownerId: {
+                type: Sequelize.INTEGER
+            },
+            shoalId: {
+                type: Sequelize.INTEGER
+            },
+            statusId: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            closedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Fishtanks');
+    }
 };
