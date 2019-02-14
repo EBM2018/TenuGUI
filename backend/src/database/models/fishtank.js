1cadf4cdc621b40885155ea39121b39b659ad7be
Lenophie <quasiphies@gmail.com>
2019-02-14 23:34:53 +0100

    Fix Fishtank model

--- backend/src/database/models/fishtank.js
+++ backend/src/database/models/fishtank.js
@@ -1,10 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Fishtank = sequelize.define('Fishtank', {
-    id: DataTypes.INTEGER
-  }, {});
-  Fishtank.associate = function(models) {
-    // associations can be defined here
-  };
-  return Fishtank;
+    const Fishtank = sequelize.define('Fishtank', {
+        ownerId: DataTypes.INTEGER,
+        shoalId: DataTypes.INTEGER,
+        statusId: DataTypes.INTEGER
+    }, {
+        timestamps: true,
+        updatedAt: false
+    });
+    Fishtank.associate = function(models) {
+        // associations can be defined here
+    };
+    return Fishtank;
 };
