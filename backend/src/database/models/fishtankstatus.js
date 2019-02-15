e9ecc63cbcd36e9849135f08b65eed8a60a0dde0
Lenophie <quasiphies@gmail.com>
2019-02-15 01:05:30 +0100

    Add seeded constants to FishtankStatus model

--- backend/src/database/models/fishtankstatus.js
+++ backend/src/database/models/fishtankstatus.js
@@ -9,5 +9,10 @@ module.exports = (sequelize, DataTypes) => {
     FishtankStatus.associate = (models) => {
         // associations can be defined here
     };
+
+    FishtankStatus.ONGOING = 1;
+    FishtankStatus.FINISHED = 2;
+    FishtankStatus.EXPIRED = 3;
+
     return FishtankStatus;
 };
