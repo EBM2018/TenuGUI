158feba34f50f13f40002bf6f493a32a830591e3
Lenophie <quasiphies@gmail.com>
2019-02-17 19:00:20 +0100

    Improve API documentation

--- backend/src/api/fishtanks/index.js
+++ backend/src/api/fishtanks/index.js
@@ -4,40 +4,28 @@ const FishtankController = require('../../controllers/fishtank.js');
 const router = new Router();
 
 /**
- * @api {get} /fishtanks Get fishtanks
- * @apiName GetFishtanks
- * @apiGroup Static Pages
- * @apiDescription This URL displays the current list of fishtanks
- * @apiSuccessExample {html} Success-Response:
- *     HTTP/1.1 200 OK
- *     Fishtanks list:
- */
-router.get('/', (req, res) => res.send('Fishtanks list:'));
-
-/**
- * @api {post} /fishtanks Post fishtank
+ * @api {post} /fishtanks Create Fishtank
  * @apiName PostFishtanks
+ * @apiGroup Fishtanks
  * @apiDescription This URL receives new fishtank declarations
  * @apiSuccessExample {html} Success-Response:
- *     HTTP/1.1 200 OK
+ * HTTP/1.1 201 OK
  */
 router.post('/', FishtankController.create);
 
 /**
- * @api {get} /fishtanks/:id Gets fishtank data
+ * @api {get} /fishtanks/:id Request Fishtank data
  * @apiName GetFishtank
- * @apiGroup Static Pages
+ * @apiGroup Fishtanks
  * @apiDescription This URL displays data about a given fishtank
- *
  * @apiParam  {Integer} id Number identifying the fishtank
  * @apiParamExample  {String} Request-Example:
  id: 1
- *
  * @apiSuccessExample {html} Success-Response:
  HTTP/1.1 200 OK
  Hello, 1!
  */
-router.get('/:id', (req, res) => res.send(`Hello, ${req.params.id}!`));
+router.get('/:id', FishtankController.read);
 
 router.use('/:id/interactions', require('./fishtankInteractions'));
 
--- backend/src/controllers/fishtank.js
+++ backend/src/controllers/fishtank.js
@@ -1,10 +1,14 @@
 const { Fishtank, FishtankStatus } = require('../database/models');
 
 module.exports = {
-  create: () => Fishtank.create({
-    ownerId: 0,
-    shoalId: 0,
-    statusId: FishtankStatus.ONGOING,
-    closedAt: null,
-  }),
+  create: (req, res) => {
+    Fishtank.create({
+      ownerId: 0,
+      shoalId: 0,
+      statusId: FishtankStatus.ONGOING,
+      closedAt: null,
+    });
+    res.status(201).send();
+  },
+  read: (req, res) => res.send(`Hello, ${req.params.id}!`),
 };
