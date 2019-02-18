9a75fc274ebd4dd48fcc45ff04abaa66f7c6eccc
Lenophie <quasiphies@gmail.com>
2019-02-18 23:46:48 +0100

    Refactor validation error handling and add auth-related checkers

--- backend/src/middlewares/fishtank.js
+++ backend/src/middlewares/fishtank.js
@@ -1,15 +1,18 @@
 const { checkSchema } = require('express-validator/check');
-const Guards = require('./guards');
+const Auth = require('./validation/customRules/checkers/auth');
 const ValidationSchema = require('./validation/schemas/fishtank.js');
 const checkCustomRules = require('./validation/customRules/checkers/fishtank.js');
-const validationErrorHandler = require('./validation/errorHandler.js');
+const bail = require('./validation/errorHandler.js');
 
 module.exports = {
   edit: [
-    Guards.isAuthenticated,
-    Guards.isOwner,
+    Auth.isAuthenticated,
+    bail(403),
     checkSchema(ValidationSchema.edit),
+    bail(422),
     checkCustomRules.edit,
-    validationErrorHandler,
+    bail(422),
+    Auth.isOwner,
+    bail(403),
   ],
 };
--- backend/src/middlewares/validation/errorHandler.js
+++ backend/src/middlewares/validation/errorHandler.js
@@ -1,7 +1,9 @@
 const { validationResult } = require('express-validator/check');
 
-module.exports = (req, res, next) => {
+const bail = (req, res, next, code) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+  if (!errors.isEmpty()) return res.status(code).json({ errors: errors.array() });
   return next();
 };
+
+module.exports = code => (req, res, next) => bail(req, res, next, code); // Nymous will be proud
