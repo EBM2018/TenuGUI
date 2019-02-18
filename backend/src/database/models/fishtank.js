7257bd5e6cfb88cf6060ac599a0c0459584fd9a2
Lenophie <quasiphies@gmail.com>
2019-02-18 02:15:30 +0100

    Fix missing attribute of Fishtank model

--- backend/src/database/models/fishtank.js
+++ backend/src/database/models/fishtank.js
@@ -3,12 +3,20 @@ module.exports = (sequelize, DataTypes) => {
     ownerId: DataTypes.INTEGER,
     shoalId: DataTypes.INTEGER,
     statusId: DataTypes.INTEGER,
+    closedAt: DataTypes.DATE,
   }, {
     timestamps: true,
     updatedAt: false,
   });
+
   Fishtank.associate = (models) => {
     Fishtank.belongsTo(models.FishtankStatus, { foreignKey: 'statusId' });
   };
+
+  Fishtank.editionTypes = {
+    CHANGE_SETTINGS: 0,
+    FINISH: 1,
+  };
+
   return Fishtank;
 };
