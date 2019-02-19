6fd29b5540ba356ece1749b95decb9597391a6ec
Lenophie <quasiphies@gmail.com>
2019-02-19 02:50:19 +0100

    Expose server

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(config.app.port, (err) => {
+module.exports = server.listen(config.app.port, (err) => {
   if (err) console.error(err);
   else console.log(`Listening on port ${config.app.port}`);
 });
