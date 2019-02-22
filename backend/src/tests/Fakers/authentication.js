15aa81d0b336fec7cf6adce17624d94420732758
Lenophie <quasiphies@gmail.com>
2019-02-22 17:37:14 +0100

    Improve fishtank creation validation test suite

--- backend/src/tests/Fakers/authentication.js
+++ backend/src/tests/Fakers/authentication.js
@@ -1,4 +1,4 @@
-const validate = (validUsers = []) => async (value, { req }) => {
+const validateAuthentication = (validUsers = []) => async (value, { req }) => {
   for (let i = 0; i < validUsers.length; i += 1) {
     if (value === validUsers[i].token) {
       req.user = validUsers[i];
@@ -8,6 +8,13 @@ const validate = (validUsers = []) => async (value, { req }) => {
   return false;
 };
 
+const isValidShoal = (validShoals = []) => async (shoalId) => {
+  for (let i = 0; i < validShoals.length; i += 1) {
+    if (shoalId === validShoals[i]) return true;
+  }
+  return false;
+};
+
 module.exports = {
-  validate,
+  validateAuthentication, isValidShoal,
 };
