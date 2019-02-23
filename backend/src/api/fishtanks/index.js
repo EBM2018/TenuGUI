e8aebfbdeb520091034f883090d4f8a5c7d995bc
Lenophie <quasiphies@gmail.com>
2019-02-23 01:11:48 +0100

    Update ApiDoc

--- backend/src/api/fishtanks/index.js
+++ backend/src/api/fishtanks/index.js
@@ -8,23 +8,35 @@ const router = new Router();
  * @api {post} /fishtanks Create Fishtank
  * @apiName PostFishtanks
  * @apiGroup Fishtanks
- * @apiDescription This URL receives new fishtank declarations
+ * @apiDescription Creates a new fishtank and returns a JSON containing its id
+ * @apiSuccess {Integer} fishtankId id of the new fishtank
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 201 OK
+ * {
+ *   "fishtankId": 3
+ * }
  */
 router.post('/', ...FishtankMiddlewares.create, FishtankController.create);
 
 /**
- * @api {get} /fishtanks/:id Request Fishtank data
+ * @api {get} /fishtanks/:id Get Fishtank
  * @apiName GetFishtank
  * @apiGroup Fishtanks
- * @apiDescription This URL displays data about a given fishtank
- * @apiParam  {Integer} id Number identifying the fishtank
+ * @apiDescription Returns a JSON containing data on a given fishtank
+ * @apiParam  {Integer} id id of a fishtank
  * @apiParamExample  {String} Request-Example:
- id: 1
+ * id: 2
  * @apiSuccessExample {json} Success-Response:
- HTTP/1.1 200 OK
- []
+ * HTTP/1.1 200 OK
+ * {
+ * "createdAt": "23/02/2019 00:06:40",
+ * "id": 2,
+ * "ownerId": 2,
+ * "shoalId": 5,
+ * "status": {
+ *   "name": "ONGOING"
+ * }
+}
  */
 router.get('/:id', FishtankController.read);
 
@@ -35,9 +47,9 @@ router.get('/:id', FishtankController.read);
  * @apiDescription This URL receives edition requests for a given fishtank
  * @apiParam  {Integer} id Number identifying the fishtank
  * @apiParamExample  {String} Request-Example:
- type: 1
+ * type: 1
  * @apiSuccessExample {json} Success-Response:
- HTTP/1.1 200 OK
+ * HTTP/1.1 200 OK
  */
 router.patch('/:id', ...FishtankMiddlewares.edit, FishtankController.edit);
 
