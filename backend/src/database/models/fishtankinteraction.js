5c13434fea5a924563d5c298dcd7d92f72100528
Lenophie <quasiphies@gmail.com>
2019-02-23 01:08:08 +0100

    Add custom getters for timestamps

--- backend/src/database/models/fishtankinteraction.js
+++ backend/src/database/models/fishtankinteraction.js
@@ -1,9 +1,30 @@
+const { formatDate } = require('../../services/formatter.js');
+
 module.exports = (sequelize, DataTypes) => {
   const FishtankInteraction = sequelize.define('FishtankInteraction', {
-    userId: DataTypes.INTEGER,
-    fishtankId: DataTypes.INTEGER,
-    typeId: DataTypes.INTEGER,
-    payload: DataTypes.JSON,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fishtankId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    typeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    payload: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      get() {
+        return formatDate(this.getDataValue('createdAt'));
+      },
+    },
   }, {
     timestamps: true,
     updatedAt: false,
