91e90f78a5f508e46475ce39f379b8a3ca560813
Whyzard <babou.lalanne@gmail.com>
2019-03-03 23:24:25 +0100

    add snapshot test for ButtonLayout.js

--- frontend/src/tests/ButtonLayout.test.js
+++ frontend/src/tests/ButtonLayout.test.js
@@ -1,3 +1,13 @@
+import ButtonLayout from '../react/Fishtank/ButtonLayout/ButtonLayout';
+import renderer from 'react-test-renderer';
+import React from 'react';
+
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('renders correctly', ()=>{
+    const component=renderer
+        .create(<ButtonLayout/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
--- frontend/src/tests/Home.test.js
+++ frontend/src/tests/Home.test.js
@@ -1,3 +1,28 @@
+
+import React from 'react';
+import Link from '../Link.react.js';
+import renderer from 'react-test-renderer';
+
+test('link changes the class when hovered', () => {
+    const component = renderer
+        .create(<Link page="http://www.facebook.com">Facebook</Link>,);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    // manually trigger the callback
+    tree.props.onMouseEnter();
+    // re-rendering
+    tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    tree.props.onMouseLeave();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+
+});
 test ('derp',()=>{
     expect('True').toBeTruthy;
 });
