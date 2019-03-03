6080bc2a8a16140031f079ea8a56e223de9847dd
Whyzard <babou.lalanne@gmail.com>
2019-03-03 23:35:32 +0100

    add snapshot test on CheckBox

--- frontend/src/tests/Checkbox.test.js
+++ frontend/src/tests/Checkbox.test.js
@@ -1,3 +1,13 @@
+import CheckBox from '../react/Widgets/Activity/CheckBox.js';
+import React from 'react';
+import renderer from 'react-test-renderer'
+
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('CheckBox renders correctly', ()=>{
+    const component=renderer
+        .create(<CheckBox id={1} reponse={"test"} fctChecked={()=>{return 'lel'}}/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
