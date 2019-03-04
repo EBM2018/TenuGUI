8d8ded2ef51c64bc1980f675253a3d399ac3e1bd
Lenophie <quasiphies@gmail.com>
2019-03-04 13:03:30 +0100

    Add fishtank interaction creation controller logic

--- backend/src/controllers/fishtankInteraction.js
+++ backend/src/controllers/fishtankInteraction.js
@@ -1,3 +1,15 @@
+const { FishtankInteraction } = require('../database/models');
+
 module.exports = {
-  create: (req, res) => res.status(201).send({}),
+  create: (req, res) => {
+    FishtankInteraction.create({
+      fishtankId: req.locals.fishtank.id,
+      userId: req.locals.user.id,
+      typeId: req.body.type,
+      payload: req.body.payload, // TODO: Sanitize payload
+    })
+      .then(() => res.status(201).send())
+      .then(() => console.log('ping owner')) // TODO: ping owner w/ websockets
+      .catch(() => res.status(500).send());
+  },
 };
