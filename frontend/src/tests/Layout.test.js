55ee588a68ea4ac8a29a0ff43e320b391cdc6cc4
Whyzard <babou.lalanne@gmail.com>
2019-03-04 08:24:58 +0100

    add snapshot testing for Layout (might need to be changed to test a layout with children)

--- frontend/src/tests/Layout.test.js
+++ frontend/src/tests/Layout.test.js
@@ -1,3 +1,12 @@
+import Layout from '../react/Layout/Layout.js';
+import React from 'react'
+import renderer from 'react-test-renderer'
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('Layout renders correctly', ()=>{
+    const component=renderer
+        .create(<Layout children={null}/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
