f8e59845dadfa62dc17ca4347f430e7c650f6d3a
Whyzard <babou.lalanne@gmail.com>
2019-03-04 09:15:52 +0100

    add snapshot testing to ActTextField, add child to Layout in snapshot testing

--- frontend/src/tests/Layout.test.js
+++ frontend/src/tests/Layout.test.js
@@ -1,12 +1,13 @@
 import Layout from '../react/Layout/Layout.js';
 import React from 'react'
 import renderer from 'react-test-renderer'
+import CheckBox from "../react/Widgets/Activity/CheckBox";
 test ('derp',()=>{
     expect('True').toBeTruthy;
 });
 test('Layout renders correctly', ()=>{
     const component=renderer
-        .create(<Layout children={null}/>);
+        .create(<Layout children={<CheckBox id={1} reponse={"42"} fctChecked={()=>{return 'This is a test'}}/>}/>);
     let tree=component.toJSON();
     expect(tree).toMatchSnapshot();
 });
