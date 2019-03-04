92789ce26bdc2d6a32219d5f13ee86ed344eb0f0
Whyzard <babou.lalanne@gmail.com>
2019-03-04 09:15:52 +0100

    add snapshot testing to ActTextField, add child to Layout in snapshot testing

--- frontend/src/tests/ActTextField.test.js
+++ frontend/src/tests/ActTextField.test.js
@@ -1,3 +1,14 @@
+import ActTextField from '../react/Widgets/Activity/ActTextField.js';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {render, fireEvent, cleanup} from 'react-testing-library';
+
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('ActTextField behaves as expected', ()=>{
+    const component=renderer
+        .create(<ActTextField id={1} text={'This is a test'}/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
