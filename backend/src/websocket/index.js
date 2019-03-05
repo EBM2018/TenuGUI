669a1b725a09b7fd8557d0f21ce0deeeaab3cc7b
Lenophie <quasiphies@gmail.com>
2019-03-05 05:36:20 +0100

    Emit in fishtank namespace when new interactions are registered

--- backend/src/websocket/index.js
+++ backend/src/websocket/index.js
@@ -10,44 +10,13 @@ const init = (server) => {
 const createSocketsNameSpaceForFishtank = (fishtankId) => {
   const newFishtankSocketsNamespace = io.of(`/fishtank-${fishtankId}`);
   newFishtankSocketsNamespace.on('connection', () => console.log(`Created namespace for fishtank ${fishtankId}`));
+  newFishtankSocketsNamespace.emit('newInteraction');
 };
 
-
-/*
-const openFishtankWebsocket = (newFishtankId) => {
-  const newFishtank = io.of(`/${newFishtankId}`);
-
-  newFishtank.on('connection', (socket) => {
-    let socketUser = null;
-    console.log('Bienvenue sur TenuGui');
-
-    socket.on('login', (logInfo) => {
-      socketUser = {
-        id: logInfo.id,
-      };
-
-      console.log(logInfo);
-
-      if (connectionType(logInfo) === 'owner') {
-        socket.join('ownerRoom');
-      } else if (connectionType(logInfo) === 'student') {
-        socket.join('studentRoom');
-      } else {
-        socket.join('spectateRoom');
-      }
-    });
-  });
-};
-
-const pingOwner = () => {
-  io.to('ownerRoom').emit('alertButton');
+const emitNewInteraction = (fishtankId) => {
+  io.of(`/fishtank-${fishtankId}`).emit('newInteraction');
 };
 
 module.exports = {
-  openFishtankWebsocket, pingOwner
-};
-*/
-
-module.exports = {
-  init, createSocketsNameSpaceForFishtank,
+  init, createSocketsNameSpaceForFishtank, emitNewInteraction,
 };
