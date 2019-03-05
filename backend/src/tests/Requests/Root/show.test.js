accf7fe39837e606f3d7d8f710cc3fd084a8a1c7
Lenophie <quasiphies@gmail.com>
2019-03-05 13:27:47 +0100

    Close Sequelize after routes retrieval tests

--- backend/src/tests/Requests/Root/show.test.js
+++ backend/src/tests/Requests/Root/show.test.js
@@ -1,8 +1,13 @@
 const request = require('supertest');
 const app = require('../../..');
+const { sequelize } = require('../../../database/models');
 const { getRequestUrlInTests } = require('../../../services/formatter.js');
 
 describe('Routes retrieval', () => {
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
   test('It should answer with a list of API urls', () => request(app)
     .get('/api')
     .expect(200)
