b622e0e3b210e454a6f1278b435422306e9d9315
Whyzard <babou.lalanne@gmail.com>
2019-03-11 01:13:11 +0100

    added minimal snapshot testing to Preview

--- frontend/src/tests/Preview.test.js
+++ frontend/src/tests/Preview.test.js
@@ -1,7 +1,13 @@
-import ActTextCheck from '../react/FishtankAdmin/Preview/Preview';
+import Preview from '../react/FishtankAdmin/Preview/Preview';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('does this render', ()=>{
+    const component= renderer
+        .create(<Preview/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
