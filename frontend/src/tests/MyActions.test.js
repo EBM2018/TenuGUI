8f893e7dd5e9a1c04a228ddf2cfb962fd3d18633
Whyzard <babou.lalanne@gmail.com>
2019-03-11 01:27:50 +0100

    added basic and sometimes useless snapshot testing to see if it improves code coverage. Will correct warnings later

--- frontend/src/tests/MyActions.test.js
+++ frontend/src/tests/MyActions.test.js
@@ -1,7 +1,14 @@
-import ActTextCheck from '../react/Dashboard/MyActions/MyActions';
+import MyActions from '../react/Dashboard/MyActions/MyActions';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('does this render', ()=> {
+    const component=renderer
+        .create(<MyActions/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
--- frontend/src/tests/Notification.test.js
+++ frontend/src/tests/Notification.test.js
@@ -1,7 +1,12 @@
-import ActTextCheck from '../react/FishtankAdmin/Notification/Notification';
+import Notification from '../react/FishtankAdmin/Notification/Notification';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('does this render', ()=>{
+    const component=renderer
+        .create(<Notification/>);
+    let tree=component.toJSON();
 });
