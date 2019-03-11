9c77cf47b454ac1195e182536b8d8c4ebb288b18
Whyzard <babou.lalanne@gmail.com>
2019-03-11 01:07:00 +0100

    add snapshot testing to UserAccountWidget.js

--- frontend/src/tests/UserAccountWidget.test.js
+++ frontend/src/tests/UserAccountWidget.test.js
@@ -1,7 +1,13 @@
-import ActTextCheck from '../react/Widgets/UserAccountWidget/UserAccountWidget';
+import UserAccountWidget from '../react/Widgets/UserAccountWidget/UserAccountWidget';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 test ('derp',()=>{
     expect('True').toBeTruthy;
+});
+test('does this render', ()=>{
+    const component=renderer
+        .create(<UserAccountWidget/>);
+    let tree=component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
