ac0c4787dff61f19498a1a19b47636c7c7459ae5
NBKZiman <leder.simon@gmail>
2019-03-19 17:34:11 +0100

    Specify namespace in 'stopButton' function

--- backend/src/websocket/index.js
+++ backend/src/websocket/index.js
@@ -4,19 +4,19 @@ const { getUser } = require('../__mock_teamy__');
 let io;
 
 // Fonction qui ajoute les evenement sur un socket Student
-const addStudentEvent = (socket) => {
+const addStudentEvent = (socket, socketUser) => {
   socket.on('stopButton', () => {
     // TODO : Faire une fonction qui renvoi le nombre de personne ayant appuyé sur le stop button
     const stopNumber = 1;
     // envoyer une alert via le socket du prof/admin
-    io.to('adminRoom').emit('alertButton', stopNumber);
+    io.of(socketUser).in('ownerRoom').emit('alertButton', stopNumber);
   });
 };
 
 // Fonction qui ajoute les evenement sur un socket Owner
-const addOwnerEvent = (socket) => {
+const addOwnerEvent = (socket, socketUser) => {
   socket.on('startActivity', (activity) => {
-    console.log(activity);
+    console.log(activity, socketUser);
     // TODO Ajout des valeurs propres a l'activité
     // TODO Ajout des events propre à l'activité
   });
@@ -26,17 +26,18 @@ const addOwnerEvent = (socket) => {
 const matchUserWithRoom = (socket, socketUser) => {
   if (socketUser.userType === 'owner') {
     socket.join('ownerRoom');
-    addOwnerEvent(socket);
+    addOwnerEvent(socket, socketUser);
   } else if (socketUser.userType === 'student') {
     socket.join('studentRoom');
-    addStudentEvent(socket);
+    addStudentEvent(socket, socketUser);
   } else {
     socket.join('spectateRoom');
   }
 };
 
-const matchSocketWithUser = (socket) => {
-  let socketUser; // info de la socket courrante, represente l'utilisateur
+const matchSocketWithUser = (socket, currentFishtank) => {
+  let socketUser;
+  socketUser.fishtank = currentFishtank; // info de la socket courrante, represente l'utilisateur
   /**
      * Catches a `socket.emit('login', token)` sent when the user authentication success response
      * is received by the front-end
@@ -61,10 +62,11 @@ const init = (server) => {
 };
 
 const createSocketsNameSpaceForFishtank = (fishtankId) => {
-  const newFishtankSocketsNamespace = io.of(`/fishtank-${fishtankId}`);
+  const currentFishtank = `/fishtank-${fishtankId}`;
+  const newFishtankSocketsNamespace = io.of(currentFishtank);
   newFishtankSocketsNamespace.on('connection', (socket) => {
     console.log(`Created namespace for fishtank ${fishtankId}`);
-    matchSocketWithUser(socket);
+    matchSocketWithUser(socket, currentFishtank);
   });
   newFishtankSocketsNamespace.emit('newInteraction');
 };
