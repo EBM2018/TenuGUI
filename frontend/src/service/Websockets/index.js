ea1eb885624a2ecfa56528fc5e9ba7966ebf79eb
NBKZiman <leder.simon@gmail>
2019-03-26 10:16:42 +0100

    Add listener on students button

--- frontend/src/service/Websockets/index.js
+++ frontend/src/service/Websockets/index.js
@@ -23,7 +23,44 @@ const addStudentEventListener = (currentSocket) => {
 };
 
 const addOwnerEventListener = (currentSocket) => {
-
+  currentSocket.on('rythme', (rythmeSelected) => {
+    // TODO : selon le cas, faire ce qui faut quand un eleve appui sur le changement de rythme
+    switch (rythmeSelected) {
+      case 'plusVite':
+        console.log("Un élève s'ennui");
+        break;
+      case 'moinsVite':
+        console.log('Un élève se perd');
+        break;
+      case 'perdu':
+        console.log('Un élève est perdu');
+        break;
+      case 'pause':
+        console.log('Un élève demande une pause');
+        break;
+      default:
+        console.log('je sais pas');
+    }
+  });
+  currentSocket.on('precision', (precisionSelected) => {
+    // TODO : Faire qqc de cette fonction
+    switch (precisionSelected) {
+      case 'plusVite':
+        console.log("Un élève s'ennui");
+        break;
+      case 'moinsVite':
+        console.log('Un élève se perd');
+        break;
+      case 'perdu':
+        console.log('Un élève est perdu');
+        break;
+      case 'pause':
+        console.log('Un élève demande une pause');
+        break;
+      default:
+        console.log('je sais pas');
+    }
+  });
 };
 
 socket.on('setToStudentRoom', () => {
