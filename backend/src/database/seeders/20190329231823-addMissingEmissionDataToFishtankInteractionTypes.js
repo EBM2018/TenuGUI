b10e835ef0b85c94925d33de5b5d43349bbfd9f6
Lenophie <quasiphies@gmail.com>
2019-03-30 01:00:19 +0100

    Fix seeder for missing data in previous fishtank interaction types

--- backend/src/database/seeders/20190329231823-addMissingEmissionDataToFishtankInteractionTypes.js
+++ backend/src/database/seeders/20190329231823-addMissingEmissionDataToFishtankInteractionTypes.js
@@ -1,24 +1,69 @@
 module.exports = {
+  // bulkUpdate doesn't work
   up: (queryInterface, { Op }) => queryInterface.sequelize.transaction(
-    t => queryInterface.bulkUpdate('FishtankInteractionTypes', {
-      emittableByAdmin: false,
-      emittableByParticipant: true,
-    }, {
-      id: { [Op.in]: [2, 3, 4] },
+    t => queryInterface.bulkDelete('FishtankInteractionTypes', {
+      id: { [Op.between]: [1, 5] },
     }, { transaction: t })
       .then(
-        () => queryInterface.bulkUpdate('FishtankInteractionTypes', {
+        () => queryInterface.bulkInsert('FishtankInteractionTypes', [{
+          id: 1,
+          name: 'ACTIVITY_CHANGE',
           emittableByAdmin: true,
           emittableByParticipant: false,
         }, {
-          id: { [Op.in]: [1, 5] },
-        }, { transaction: t }),
+          id: 2,
+          name: 'EMERGENCY_PRESS',
+          emittableByAdmin: false,
+          emittableByParticipant: true,
+        }, {
+          id: 3,
+          name: 'FEEDBACK_SUBMIT',
+          emittableByAdmin: false,
+          emittableByParticipant: true,
+        }, {
+          id: 4,
+          name: 'COOL_PRESS',
+          emittableByAdmin: false,
+          emittableByParticipant: true,
+        }, {
+          id: 5,
+          name: 'PERIOD_CHANGE',
+          emittableByAdmin: true,
+          emittableByParticipant: false,
+        }], { transaction: t }),
       ),
   ),
-  down: (queryInterface, { Op }) => queryInterface.bulkUpdate('FishtankInteractionTypes', {
-    emittableByAdmin: false,
-    emittableByParticipant: false,
-  }, {
-    id: { [Op.between]: [1, 5] },
-  }),
+  down: (queryInterface, { Op }) => queryInterface.sequelize.transaction(
+    t => queryInterface.bulkDelete('FishtankInteractionTypes', {
+      id: { [Op.between]: [1, 5] },
+    }, { transaction: t })
+      .then(
+        () => queryInterface.bulkInsert('FishtankInteractionTypes', [{
+          id: 1,
+          name: 'ACTIVITY_CHANGE',
+          emittableByAdmin: false,
+          emittableByParticipant: false,
+        }, {
+          id: 2,
+          name: 'EMERGENCY_PRESS',
+          emittableByAdmin: false,
+          emittableByParticipant: false,
+        }, {
+          id: 3,
+          name: 'FEEDBACK_SUBMIT',
+          emittableByAdmin: false,
+          emittableByParticipant: false,
+        }, {
+          id: 4,
+          name: 'COOL_PRESS',
+          emittableByAdmin: false,
+          emittableByParticipant: false,
+        }, {
+          id: 5,
+          name: 'PERIOD_CHANGE',
+          emittableByAdmin: false,
+          emittableByParticipant: false,
+        }], { transaction: t }),
+      ),
+  ),
 };
