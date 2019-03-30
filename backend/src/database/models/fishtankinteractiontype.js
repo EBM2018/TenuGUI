d856165c4caf01752c489925a95f36215a17a287
Lenophie <quasiphies@gmail.com>
2019-03-30 01:00:48 +0100

    Update enums in FishtankInteractionType model

--- backend/src/database/models/fishtankinteractiontype.js
+++ backend/src/database/models/fishtankinteractiontype.js
@@ -7,11 +7,32 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false,
   });
 
-  FishtankInteractionType.ACTIVITY_CHANGE = 1;
-  FishtankInteractionType.EMERGENCY_PRESS = 2;
-  FishtankInteractionType.FEEDBACK_SUBMIT = 3;
-  FishtankInteractionType.COOL_PRESS = 4;
-  FishtankInteractionType.PERIOD_CHANGE = 5;
+  FishtankInteractionType.ADMIN = {
+    ACTIVITY_CHANGE: 1,
+    PERIOD_CHANGE: 5,
+    FEEDBACK_ASK: 19,
+    ATTENTION_ASK: 17,
+    UNDERSTANDING_ASK: 18,
+    SUMMARY_ASK: 20,
+    PROGRESSION_ASK: 21,
+  };
+
+  FishtankInteractionType.PARTICIPANT = {
+    EMERGENCY_PRESS: 2,
+    FEEDBACK_SUBMIT: 3,
+    COOL_PRESS: 4,
+    QUESTION_ASK: 6,
+    FASTER_RHYTHM_ASK: 7,
+    SLOWER_RHYTHM_ASK: 8,
+    LOW_UNDERSTANDING_NOTIFY: 9,
+    PAUSE_ASK: 10,
+    EXPLANATIONS_ASK: 11,
+    DETAILS_ASK: 12,
+    EXAMPLES_ASK: 13,
+    TRIVIA_ASK: 14,
+    REFERENCE_ASK: 15,
+    EXERCISE_ASK: 16,
+  };
 
   return FishtankInteractionType;
 };
